a1847b822d1e336b4da94b4373e933e44cbaffac
Bernard Ograh <baograh@st.knust.edu.gh>
2024-02-09 00:22:38 +0000

    GET /hello added successfully

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express()
 const PORT = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Welcome to Express App')
+})
+
+app.get('/hello', (req, res) => {
+    res.send('This is the hello endpoint')
 })
 
 app.listen(PORT, () => {
