80d5a0a9e62014d45927f1aec7f1736b427b3645
Bernard Ograh <baograh@st.knust.edu.gh>
2024-02-12 22:57:05 +0000

    GET by id method created successfully

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ app.get('/todos', (req, res) => {
     res.json(todos);
 });
 
+// Get single todo item by id
+app.get('/todos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const todo = todos.find(todo => todo.id === id);
+
+    if (!todo) {
+        res.status(404).json({ message: "Todo not found" });
+    } else {
+        res.json(todo);
+    }
+});
+
 // Create todo
 app.post('/todos', (req, res) => {
     const todo = req.body;
